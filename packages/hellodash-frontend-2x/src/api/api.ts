189254623851930e3d224f779062672549e1935b
Jeff Manke <jeffman879@gmail.com>
2022-07-11 10:09:09 -0700

    Merge pull request #1 from jmanke/main
    ...

--- packages/hellodash-frontend-2x/src/api/api.ts
+++ packages/hellodash-frontend-2x/src/api/api.ts
@@ -1,116 +1,96 @@
-import axios, { AxiosError } from 'axios'
-import qs from 'qs'
-import appState from '../stores/app-state'
-import CancelationToken from './cancellation-token'
+import axios, { AxiosError } from 'axios';
+import qs from 'qs';
+import appState from '../stores/app-state';
+import CancelationToken from './cancellation-token';
 
 //#region interfaces
 
 export interface PaginationQuery {
-  pageNumber: number
-  pageSize: number
+  pageNumber: number;
+  pageSize: number;
 }
 
 export interface PagedResponse<T> {
-  data: T
-  pageNumber: number
-  pageSize: number
-  totalPages: number
-  totalRecords: number
+  data: T;
+  pageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  totalRecords: number;
 }
 
 //#endregion
 
 //#region constants
 
-export const baseApiUrl = (): string => 'http://localhost:5000/api' //process.env.LIVE_SERVER ? 'https://hellodash-server-2x.herokuapp.com/api' : 'http://localhost:5000/api';
+export const baseApiUrl = (): string => (process.env.LIVE_SERVER ? 'https://hellodash-server-2x.herokuapp.com/api' : 'http://localhost:5000/api');
 
 //#endregion
 
 //#region REST functions
 
-export async function get<T>({
-  url,
-  config,
-  cancelationToken,
-}: {
-  url: string
-  config?: any
-  cancelationToken?: CancelationToken
-}): Promise<T | null> {
+export async function get<T>({ url, config, cancelationToken }: { url: string; config?: any; cancelationToken?: CancelationToken }): Promise<T | null> {
   return axios
     .get<T>(createUrl(url), await configWithAuthAndParams(config))
-    .then((response) => {
+    .then(response => {
       if (cancelationToken?.cancelled) {
-        return null
+        return null;
       }
 
       if (response.status === 200) {
-        return response.data
+        return response.data;
       } else {
-        return null
+        return null;
       }
     })
     .catch((error: AxiosError) => {
-      handleError(error)
+      handleError(error);
 
-      throw error
-    })
+      throw error;
+    });
 }
 
-export async function post<T, U>(
-  url: string,
-  data: T,
-  config?: any,
-): Promise<U> {
+export async function post<T, U>(url: string, data: T, config?: any): Promise<U> {
   return axios
     .post<U>(createUrl(url), data, await configWithAuth(config))
-    .then((response) => response.data)
+    .then(response => response.data)
     .catch((error: AxiosError) => {
-      handleError(error)
+      handleError(error);
 
-      throw error
-    })
+      throw error;
+    });
 }
 
-export async function put<T, U>(
-  url: string,
-  data: T,
-  config?: any,
-): Promise<U> {
+export async function put<T, U>(url: string, data: T, config?: any): Promise<U> {
   return axios
     .put<U>(createUrl(url), data, await configWithAuth(config))
-    .then((response) => response.data)
+    .then(response => response.data)
     .catch((error: AxiosError) => {
-      handleError(error)
+      handleError(error);
 
-      throw error
-    })
+      throw error;
+    });
 }
 
-export async function patch<T, U>(
-  url: string,
-  data?: T,
-  config?: any,
-): Promise<U> {
+export async function patch<T, U>(url: string, data?: T, config?: any): Promise<U> {
   return axios
     .patch<U>(createUrl(url), data, await configWithAuth(config))
-    .then((response) => response.data)
+    .then(response => response.data)
     .catch((error: AxiosError) => {
-      handleError(error)
+      handleError(error);
 
-      throw error
-    })
+      throw error;
+    });
 }
 
 export async function remove<T>(url: string, config?: any): Promise<T> {
   return axios
     .delete<T>(createUrl(url), await configWithAuth(config))
-    .then((response) => response.data)
+    .then(response => response.data)
     .catch((error: AxiosError) => {
-      handleError(error)
+      handleError(error);
 
-      throw error
-    })
+      throw error;
+    });
 }
 
 //#endregion
@@ -118,7 +98,7 @@ export async function remove<T>(url: string, config?: any): Promise<T> {
 //#region api helper functions
 
 function createUrl(url: string): string {
-  return `${baseApiUrl()}/${url}`
+  return `${baseApiUrl()}/${url}`;
 }
 
 async function configWithAuthAndParams(config: Record<string, any>) {
@@ -128,9 +108,9 @@ async function configWithAuthAndParams(config: Record<string, any>) {
       Authorization: `Bearer ${await appState.authClient.getTokenSilently()}`,
     },
     paramsSerializer: (params: string) => {
-      return qs.stringify(params, { arrayFormat: 'repeat' })
+      return qs.stringify(params, { arrayFormat: 'repeat' });
     },
-  }
+  };
 }
 
 async function configWithAuth(config: Record<string, any>) {
@@ -139,7 +119,7 @@ async function configWithAuth(config: Record<string, any>) {
     headers: {
       Authorization: `Bearer ${await appState.authClient.getTokenSilently()}`,
     },
-  }
+  };
 }
 
 // function triggerErrorNotification(err: Error): void {
@@ -160,7 +140,7 @@ async function configWithAuth(config: Record<string, any>) {
 
 function handleError(error: AxiosError) {
   if (error.response?.status === 401) {
-    window.location.reload()
+    window.location.reload();
   } else if (!axios.isCancel(error)) {
     // triggerErrorNotification(error);
   }
