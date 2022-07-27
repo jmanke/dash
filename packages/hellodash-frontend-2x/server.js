9c767455d8515321d31c3546e9ec2e082c4399d3
jmanke <jeffman879@gmail.com>
2022-07-26 21:15:28 -0700

    Merge branch 'main' into production

--- packages/hellodash-frontend-2x/server.js
+++ packages/hellodash-frontend-2x/server.js
@@ -1,15 +1,22 @@
+const express = require('express');
 const handler = require('serve-handler');
-const http = require('http');
 
-const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/vercel/serve-handler#options
+const app = express();
+const port = process.env.PORT || 3333;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
+}
+
+app.use((request, response) => {
   return handler(request, response, {
-    public: 'www/'
+    public: 'www/',
   });
 });
 
-const port = process.env.PORT || 3333
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`);
 });
