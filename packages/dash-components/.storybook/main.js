10b43f55d5b44010c95ffda6b1e682ea93e0e5b1
Jeff Manke <jmanke879@gmail.com>
2022-10-12 18:44:23 -0700

    ...

--- packages/dash-components/.storybook/main.js
+++ packages/dash-components/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@luigiminardim/storybook-addon-globals-controls'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/web-components',
   staticDirs: ['../www'],
 };
