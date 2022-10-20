e4f823aba75c345aff95d3b1a71f43617f2c4816
Jeff Manke <jeffman879@gmail.com>
2022-10-20 15:07:58 -0700

    fix keyboard interaction for logout button

--- packages/hellodash-frontend-2x/src/components/common/dash-profile-settings/dash-profile-settings.tsx
+++ packages/hellodash-frontend-2x/src/components/common/dash-profile-settings/dash-profile-settings.tsx
@@ -45,7 +45,7 @@ export class DashProfileSettings {
           <dash-dropdown placement='bottom-end' placement-strategy='fixed' autoClose>
             <dash-icon-button slot='dropdown-trigger' iconUrl={this.user.picture} icon='person' scale='l' rounded></dash-icon-button>
             <dash-list selection-mode='none'>
-              <dash-list-item onClick={() => logout(appState.authClient)}>Logout</dash-list-item>
+              <dash-list-item onDashListItemSelectedChanged={() => logout(appState.authClient)}>Logout</dash-list-item>
             </dash-list>
           </dash-dropdown>
         </Host>
