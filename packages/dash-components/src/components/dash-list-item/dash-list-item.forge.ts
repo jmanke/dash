d63c2a270b3c97a619937d51a7104bb2d184496a
Jeff Manke <jeffman879@gmail.com>
2022-12-03 17:45:59 -0800

    list item forge

---
+++ packages/dash-components/src/components/dash-list-item/dash-list-item.forge.ts
@@ -0,0 +1,27 @@
+import { html } from 'lit-html';
+
+const template = (args, updateArg) =>
+  html`<dash-list-item
+    selection-mode=${args.selectionMode}
+    ?selected=${args.selected}
+    ?disabled=${args.disabled}
+    @dashListItemSelectedChanged=${() => updateArg('selected', !args.selected)}
+    >List item</dash-list-item
+  > `;
+
+export const listItemDefinition = {
+  name: 'List item',
+  controls: {
+    disabled: { type: 'boolean' },
+    selected: { type: 'boolean' },
+    selectionMode: { type: 'select', options: ['single', 'multiple', 'none'] },
+  },
+  template,
+  args: {
+    disabled: false,
+    selected: false,
+    selectionMode: 'single',
+  },
+};
+
+export default listItemDefinition;
