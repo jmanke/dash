f28dfbd1cf98d170595c72c404e9cc464c5ee0c0
Jeff Manke <jeffman879@gmail.com>
2022-12-03 17:03:17 -0800

    dropdown forge

---
+++ packages/dash-components/src/components/dash-dropdown/dash-dropdown.forge.ts
@@ -0,0 +1,28 @@
+import { html } from 'lit-html';
+import { placementControl, placementStrategyControl } from '../../../.forge/common/controls';
+
+const template = args => html` <dash-dropdown style="margin: 8rem;" placement=${args.placement} placement-strategy=${args.placementStrategy} ?auto-close=${args.autoClose}>
+  <dash-button slot="dropdown-trigger">Open me</dash-button>
+
+  <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>
+  <div>Item 4</div>
+</dash-dropdown>`;
+
+export const dropdownDefinition = {
+  name: 'Dropdown',
+  controls: {
+    placement: placementControl,
+    placementStrategy: placementStrategyControl,
+    autoClose: { type: 'boolean' },
+  },
+  template,
+  args: {
+    placement: 'bottom-end',
+    placementStrategy: 'absolute',
+    autoClose: true,
+  },
+};
+
+export default dropdownDefinition;
