f0b28d9234dbf1f81e73a6ac90a0f93f80adb2ef
Jeff Manke <jeffman879@gmail.com>
2022-12-04 16:56:59 -0800

    ...

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const handler = require('serve-handler');
+const express = require("express");
+const handler = require("serve-handler");
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}${req.url}`);
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
     else next();
   });
 }
 
 app.use((request, response) => {
   return handler(request, response, {
-    public: 'packages/hellodash-frontend-2x/www/',
+    public: "packages/hellodash/www/",
   });
 });
 
