e15c56450472fb17b61417dde49a34a5b45a9269
Jeff Manke <jeffman879@gmail.com>
2022-12-21 12:15:49 -0800

    ...

--- packages/dash-components/src/components/dash-event-calendar/dash-event-calendar.tsx
+++ packages/dash-components/src/components/dash-event-calendar/dash-event-calendar.tsx
@@ -1,9 +1,10 @@
-import { Component, Host, h, State } from '@stencil/core';
-import { DateTime } from 'luxon';
+import { Component, Host, h, State, Watch } from '@stencil/core';
+import { DateTime, Info } from 'luxon';
 
-const WEEKDAYS = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-const MONTH = 'December';
-const YEAR = '2022';
+interface Day {
+  day: number;
+  month: number;
+}
 
 @Component({
   tag: 'dash-event-calendar',
@@ -19,7 +20,14 @@ export class DashEventCalendar {
 
   //#region @State
   @State()
-  weeks: number[][] = [];
+  weeks: Day[][] = [];
+
+  @State()
+  date: DateTime;
+  @Watch('date')
+  dateChanged() {
+    this.updateCalendar();
+  }
   //#endregion
 
   //#region @Prop
@@ -30,7 +38,8 @@ export class DashEventCalendar {
 
   //#region Component lifecycle
   componentWillLoad() {
-    this.updateMonth(12, 2022);
+    this.date = DateTime.fromObject({ year: 2022, month: 12 });
+    this.updateCalendar();
   }
   //#endregion
 
@@ -42,12 +51,12 @@ export class DashEventCalendar {
 
   //#region Local methods
 
-  updateMonth(month: number, year: number) {
-    const monthDt = DateTime.fromObject({ year, month });
-    const daysInMonth = monthDt.daysInMonth;
+  updateCalendar() {
+    const date = this.date;
+    const daysInMonth = date.daysInMonth;
 
     // map startDay from Mon - Sun to Sun - Sat
-    let startDay = monthDt.startOf('month').weekday + 1;
+    let startDay = date.startOf('month').weekday + 1;
     if (startDay === 8) {
       startDay = 1;
     }
@@ -57,14 +66,24 @@ export class DashEventCalendar {
     // pre-populate array of weeks with null values
     const weeks = Array.from(Array(numWeeks), () => new Array(7).fill(null));
     // starting day always 1
-    let day = 1;
+    let currDate = date.minus({ days: startDay - 1 });
 
-    for (let i = startDay - 1; day <= daysInMonth; i++) {
+    // generate 2d array of days
+    for (let i = 0; i < numWeeks * 7; i++) {
       const weekNum = Math.floor(i / 7);
-      weeks[weekNum][i % 7] = day++;
+      weeks[weekNum][i % 7] = { day: currDate.day, month: currDate.month };
+      currDate = currDate.plus({ days: 1 });
     }
 
     this.weeks = weeks;
+  }
+
+  prevMonth() {
+    this.date = this.date.minus({ months: 1 });
+  }
+
+  nextMonth() {
+    this.date = this.date.plus({ months: 1 });
   }
 
   //#endregion
@@ -75,21 +94,24 @@ export class DashEventCalendar {
         <div class='calendar'>
           <div class='header'>
             <h3 class='title'>
-              {MONTH} {YEAR}
+              {this.date.toLocaleString({ month: 'long' })} {this.date.year}
             </h3>
 
-            
+            <span>
+              <dash-icon-button icon='chevron-left' onClick={this.prevMonth.bind(this)}></dash-icon-button>
+              <dash-icon-button icon='chevron-right' onClick={this.nextMonth.bind(this)}></dash-icon-button>
+            </span>
           </div>
           <table class='container'>
             <tr>
-              {WEEKDAYS.map(d => (
+              {Info.weekdays('short').map(d => (
                 <th>{d}</th>
               ))}
             </tr>
             {this.weeks.map(week => (
               <tr>
                 {week.map(day => (
-                  <td>{day}</td>
+                  <td class={this.date.month === day.month ? undefined : 'faded'}>{day.day}</td>
                 ))}
               </tr>
             ))}
