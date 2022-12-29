07927e67b5d04247ac1278811a4fb8c805df923b
Jeff Manke <jeffman879@gmail.com>
2022-12-28 23:46:18 -0800

    update padding for dropdowns hellodash

--- packages/hellodash/src/components/common/dash-profile-settings/dash-profile-settings.tsx
+++ packages/hellodash/src/components/common/dash-profile-settings/dash-profile-settings.tsx
@@ -44,6 +44,7 @@ export class DashProfileSettings {
         <Host>
           <dash-dropdown placement='bottom-end' placement-strategy='fixed' autoClose>
             <dash-icon-button slot='dropdown-trigger' iconUrl={this.user.picture} icon='person' scale='l' rounded></dash-icon-button>
+            
             <dash-list selection-mode='none'>
               <dash-list-item onDashListItemSelectedChanged={() => logout(appState.authClient)}>Logout</dash-list-item>
             </dash-list>
