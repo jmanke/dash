f420bbb0193da35c780d7ebc73998dcf0949fb38
Jeff Manke <jeffman879@gmail.com>
2023-02-20 18:48:07 -0800

    init

---
+++ packages/hellodash-vue/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script lang="tsx">
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+      count: ref(0),
+    };
+  },
+  render() {
+    return (
+      <>
+        <h1 class="header">{this.msg}</h1>
+        <dash-button onClick={() => this.count++}>{this.count}</dash-button>
+      </>
+    );
+  },
+});
+</script>
+
+<style scoped>
+.header {
+  color: red;
+}
+</style>
