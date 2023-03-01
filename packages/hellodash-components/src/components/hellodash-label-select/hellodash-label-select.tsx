4de2fd17fb2ac782cff851d4d6aecebc6afff290
Jeff Manke <jeffman879@gmail.com>
2023-03-01 14:11:40 -0800

    comments

--- packages/hellodash-components/src/components/hellodash-label-select/hellodash-label-select.tsx
+++ packages/hellodash-components/src/components/hellodash-label-select/hellodash-label-select.tsx
@@ -14,6 +14,7 @@ export class HellodashLabelEdit {
 
   colorSwatches = new Map<number, HTMLDashColorSwatchElement>();
 
+  /** Label being edited */
   editingLabel: Label;
 
   //#endregion
@@ -26,12 +27,16 @@ export class HellodashLabelEdit {
 
   //#region @State
 
+  /** Map of label id to label */
   @State() labelsMap: Map<number, Label>;
 
+  /** Ids of filtered labels */
   @State() filteredLabelsIds: Set<number> = new Set();
 
+  /** Value to filter labels by */
   @State() filterValue: string;
 
+  /** Whether the drill menu is active */
   @State() drillMenuActive: boolean;
   @Watch('drillMenuActive')
   drillMenuActiveChanged(drillMenuActive) {
@@ -45,16 +50,28 @@ export class HellodashLabelEdit {
 
   //#region @Prop
 
+  /**
+   * Labels to display
+   */
   @Prop() labels: Label[] = [];
   @Watch('labels')
   labelsChanged() {
     this.updateLabelsMap();
   }
 
+  /**
+   * All labels to choose from
+   */
   @Prop() allLabels: Label[] = [];
 
+  /**
+   * When true, the user can create new labels
+   */
   @Prop({ reflect: true }) canCreateLabel: boolean;
 
+  /**
+   * When true, the dropdown will be focused on open
+   */
   @Prop({ reflect: true }) autoFocus: boolean;
 
   //#endregion
@@ -93,10 +110,18 @@ export class HellodashLabelEdit {
 
   //#region Local methods
 
+  /**
+   * Updates the labels map
+   */
   updateLabelsMap() {
     this.labelsMap = this.labels.reduce((map, label) => map.set(label.id, { ...label }), new Map<number, Label>());
   }
 
+  /**
+   * Handles when the selected label changes
+   * @param label Label that was changed
+   * @param isSelected Whether the label is selected
+   */
   selectedLabelChanged(label: Label, isSelected: Boolean) {
     if (isSelected) {
       this.labelAdded.emit(label);
@@ -106,6 +131,9 @@ export class HellodashLabelEdit {
     this.labelRemoved.emit(label);
   }
 
+  /**
+   * Creates a new label
+   */
   async createLabel() {
     if (!this.canCreateLabel) {
       return;
@@ -117,6 +145,11 @@ export class HellodashLabelEdit {
     this.filterElement?.select();
   }
 
+  /**
+   * Handles when label color changed
+   * @param label Label that was changed
+   * @param color New color
+   */
   labelColorChanged(label: Label, color: Color) {
     this.labelUpdated.emit({
       ...label,
@@ -124,6 +157,10 @@ export class HellodashLabelEdit {
     });
   }
 
+  /**
+   * Start editing label
+   * @param label Label to edit
+   */
   editLabel(label: Label) {
     this.editingLabel = label;
     this.drillMenuActive = true;
